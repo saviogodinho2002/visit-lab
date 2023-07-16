285aa27f39cd48daa379e30bbab9a852097ef147
saviogodinho2002 <saviogmoiagaia.2002@gmail.com>
2023-07-16 00:26:09 -0300

    fix: registro unico para usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string("register");
+            $table->string("register")->unique();
 
             $table->rememberToken();
             $table->timestamps();
